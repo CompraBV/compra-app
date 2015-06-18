5c2a820c27315938d5f6427824b4f1750f104791
bobdesaunois <bobdesaunois@gmail.com>
2015-06-18 11:07:20 +0200

    App progress

--- src/main/java/nl/compra/compraapp/MainActivity.java
+++ src/main/java/nl/compra/compraapp/MainActivity.java
@@ -1,19 +1,27 @@
 package nl.compra.compraapp;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v7.app.ActionBarActivity;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TableLayout;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -33,6 +41,77 @@ public class MainActivity extends ActionBarActivity {
         Toast toast = Toast.makeText(context, text, duration);
 
         toast.show();
+
+        //Spinners
+
+        Spinner spinnerDomeinen = (Spinner) findViewById(R.id.domeinen);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.catagories, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerDomeinen.setAdapter(adapter);
+
+        Spinner spinnerSorteringen = (Spinner) findViewById(R.id.sorteringen);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterrrrrrrrrrr = ArrayAdapter.createFromResource(this,
+                R.array.sort, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterrrrrrrrrrr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerSorteringen.setAdapter(adapterrrrrrrrrrr);
+
+        addDomainToTable();
+
+    }
+
+    public void addDomainToTable ()
+    {
+
+        TableLayout placeHolder = (TableLayout) findViewById(R.id.domainRowsTable);
+        getLayoutInflater().inflate(R.layout.domain_row, placeHolder);
+
+        try
+        {
+
+            String url = "https://www.compra.nl/?c=api&m=getExtensions";
+            JSONObject jsonObject = new JSONObject (url);
+            System.out.println ("JSON OPJECT WOOO");
+//            System.out.println ("WASD WASD WTF I CAN'T MOVE");
+//            System.out.println (jsonObject);
+
+//        } catch (java.io.IOException e) {
+//
+//            e.printStackTrace();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // This code was copied from the internet.
+    // Source: http://stackoverflow.com/questions/8616781/how-to-get-a-web-pages-source-code-from-java
+    private String getUrlSource(String url) throws IOException
+    {
+
+        URL custUrl = null;
+        try {
+            custUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        URLConnection yc = custUrl.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                yc.getInputStream(), "UTF-8"));
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            a.append(inputLine);
+        in.close();
+
+        return a.toString();
 
     }
 
