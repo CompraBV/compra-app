4c887d53aee502acdddf555cf94b0d165bcc5790
bobdesaunois <bobdesaunois@gmail.com>
2015-07-16 10:47:45 +0200

    3num3r4t0rz

---
+++ src/main/java/nl/compra/compraapp/DomainSortingModes.java
@@ -0,0 +1,16 @@
+package nl.compra.compraapp;
+
+/**
+ * Created by Bob Desaunois on 16-7-2015.
+ */
+public enum DomainSortingModes {
+
+    ALL,
+    POPULAR,
+    NEW,
+    EUROPE,
+    COUNTRIES,
+    COMMON,
+    SPECIAL_OFFERS;
+
+}
---
+++ src/main/java/nl/compra/compraapp/ExtensionSortingModes.java
@@ -0,0 +1,13 @@
+package nl.compra.compraapp;
+
+/**
+ * Created by Bob Desaunois on 16-7-2015.
+ */
+public enum ExtensionSortingModes {
+
+    PRICE_ASCENDING,
+    PRICE_DESCENDING,
+    ALPHABETIC_ASCENDING,
+    ALPHABETIC_DESCENDING;
+
+}
