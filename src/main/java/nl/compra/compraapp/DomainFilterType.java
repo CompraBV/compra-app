ba021624eb3bece100412f242de66c665c011008
bobdesaunois <bobdesaunois@gmail.com>
2015-07-17 15:24:04 +0200

    Again with the renames

--- src/main/java/nl/compra/compraapp/DomainFilterType.java
+++ src/main/java/nl/compra/compraapp/DomainFilterType.java
@@ -3,7 +3,7 @@ package nl.compra.compraapp;
 /**
  * Created by Bob Desaunois on 16-7-2015.
  */
-public enum DomainFilters {
+public enum DomainFilterType {
 
     ALL,
     POPULAR,
--- src/main/java/nl/compra/compraapp/ExtensionSortingType.java
+++ src/main/java/nl/compra/compraapp/ExtensionSortingType.java
@@ -3,7 +3,7 @@ package nl.compra.compraapp;
 /**
  * Created by Bob Desaunois on 16-7-2015.
  */
-public enum ExtensionSorting {
+public enum ExtensionSortingType {
 
     PRICE_ASCENDING,
     PRICE_DESCENDING,
