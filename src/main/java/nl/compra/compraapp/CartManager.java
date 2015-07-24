ae5286ebcdd7d1b905738c31edb3e340dd1c177d
bobdesaunois <bobdesaunois@gmail.com>
2015-07-24 16:58:17 +0200

    Cart handling progress

---
+++ src/main/java/nl/compra/compraapp/CartManager.java
@@ -0,0 +1,17 @@
+package nl.compra.compraapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Bob Desaunois on 24-7-2015.
+ */
+public class CartManager {
+
+    public static List<Domain> cart = new ArrayList<Domain> ();
+
+    public static Domain get (int index) { return cart.get (index); }
+
+    public static void addToCart (Domain domain) { cart.add (domain); }
+
+}
